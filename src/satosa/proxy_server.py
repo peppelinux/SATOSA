dade6c3aaeb7325d9e646418df944548fabac3e7
wert <wert@debian10-pysaml-ldap2.lan>
2019-04-14 00:56:31 +0200

    Added DecideBackendByTarget Microservice

--- src/satosa/proxy_server.py
+++ src/satosa/proxy_server.py
@@ -12,7 +12,7 @@ from .context import Context
 from .response import ServiceError, NotFound
 from .routing import SATOSANoBoundEndpointError
 from saml2.s_utils import UnknownSystemEntity
-from .util import repr_saml
+
 
 logger = logging.getLogger(__name__)
 
@@ -43,11 +43,6 @@ def unpack_post(environ, content_length):
         data = json.loads(post_body)
 
     logger.debug("unpack_post:: %s", data)
-
-    # fancy saml representation
-    saml_data = data.get('SAMLRequest') or data.get('SAMLResponse') or ''
-    logger.debug("read unpackacked post: {}".format(repr_saml(saml_data.encode(), b64=True)))
-
     return data
 
 
@@ -64,10 +59,6 @@ def unpack_request(environ, content_length=0):
         data = unpack_post(environ, content_length)
 
     logger.debug("read request data: %s", data)
-
-    # fancy saml representation
-    saml_data = data.get('SAMLRequest') or data.get('SAMLResponse') or ''
-    logger.debug("saml request data: %s", repr_saml(saml_data.encode(), b64=True))
     return data
 
 
