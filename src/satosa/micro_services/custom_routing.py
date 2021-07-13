fcafbe44ca4fd819ed443fa3350428c73b49e5cf
Giuseppe De Marco <giuseppe.demarco@teamdigitale.governo.it>
2021-07-13 03:12:59 +0200

    chore: context.decorate in target routing ms
    Co-authored-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- src/satosa/micro_services/custom_routing.py
+++ src/satosa/micro_services/custom_routing.py
@@ -82,7 +82,7 @@ class DecideBackendByTarget(RequestMicroService):
 
     def _rewrite_context(self, entity_id, context) -> None:
         tr_backend = self._get_backend(context, entity_id)
-        context.internal_data['target_entity_id'] = entity_id
+        context.decorate(Context.KEY_TARGET_ENTITYID, entity_id)
         context.target_frontend = context.target_frontend or context.state.get('ROUTER')
         native_backend = context.target_backend
         msg = (f'Found DecideBackendByTarget ({self.name} microservice) '
